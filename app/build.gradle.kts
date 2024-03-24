744ff8a1302f01fc305470b002727d785f3f60b6
npt306 <nptai212@clc.fitus.edu.vn>
2024-03-25 04:04:11 +0700

    save usernname

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    alias(libs.plugins.androidApplication)
     id("com.google.gms.google-services")
 }
 
@@ -34,18 +34,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
--- app/src/main/java/com/example/geoshare/MainActivity.java
+++ app/src/main/java/com/example/geoshare/MainActivity.java
@@ -1,62 +1,93 @@
 package com.example.geoshare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    Button buttonLogout;
-    TextView textView;
+    Button buttonLogout, buttonSend;
+    TextView textView, textViewResult;
+    EditText editTextSend;
     FirebaseUser firebaseUser;
     ImageButton imageButtonProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
-        imageButtonProfile = findViewById(R.id.btnProfile);
-        imageButtonProfile.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogout = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        buttonSend = findViewById(R.id.send_data);
+        textViewResult = findViewById(R.id.result);
+        editTextSend = findViewById(R.id.data_test);
+
+        firebaseUser = mAuth.getCurrentUser();
+
+        if(firebaseUser == null){
+            Intent intent = new Intent(getApplicationContext(), SignIn.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(firebaseUser.getEmail());
+        }
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
                 startActivity(intent);
                 finish();
             }
         });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String send_data = String.valueOf(editTextSend.getText());
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
 
-
-//        mAuth = FirebaseAuth.getInstance();
-//        buttonLogout = findViewById(R.id.logout);
-//        textView = findViewById(R.id.user_details);
-//        firebaseUser = mAuth.getCurrentUser();
-//        if(firebaseUser == null){
-//            Intent intent = new Intent(getApplicationContext(), SignIn.class);
-//            startActivity(intent);
-//            finish();
-//        }
-//        else {
-//            textView.setText(firebaseUser.getEmail());
-//        }
-//        buttonLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FirebaseAuth.getInstance().signOut();
-//                Intent intent = new Intent(getApplicationContext(), SignIn.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
+                myRef.setValue(send_data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // Gửi dữ liệu thành công
+                                Log.d("Firebase", "Data sent successfully!");
+                                textViewResult.setText("successfully");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Gửi dữ liệu thất bại
+                                Log.e("Firebase", "Failed to send data: " + e.getMessage());
+                                // Xử lý nguyên nhân thất bại tại đây
+                                textViewResult.setText(e.getMessage());
+                            }
+                        });
+            }
+        });
 
     }
 }
