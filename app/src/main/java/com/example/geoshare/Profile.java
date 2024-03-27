2fce09b658feac9bd22323df627f1e62e6f84307
npt306 <nptai212@clc.fitus.edu.vn>
2024-03-27 10:43:29 +0700

    logout update

--- app/src/main/java/com/example/geoshare/Profile.java
+++ app/src/main/java/com/example/geoshare/Profile.java
@@ -3,22 +3,36 @@ package com.example.geoshare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Profile extends AppCompatActivity {
     ImageButton buttonProfileBack;
+    Button buttonLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         buttonProfileBack = findViewById(R.id.back_button);
+        buttonLogout = findViewById(R.id.btnLogout);
         buttonProfileBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
                 startActivity(intent);
                 finish();
             }
