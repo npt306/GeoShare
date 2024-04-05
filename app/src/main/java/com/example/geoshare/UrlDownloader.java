6a1f72c42e40a9de2c737b51be22058beddeac58
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-04-05 19:41:54 +0700

    refractor: create UrlDownloader class with methods from MainActivity

---
+++ app/src/main/java/com/example/geoshare/UrlDownloader.java
@@ -0,0 +1,80 @@
+package com.example.geoshare;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UrlDownloader extends AsyncTask<String, Void, String>{
+    MainActivity callerContext;
+
+    public UrlDownloader(Context callerContext){
+        this.callerContext= (MainActivity) callerContext;
+    }
+
+    @Override
+    protected String doInBackground(String... url) {
+
+        String data = "";
+
+        try {
+            data = downloadUrl(url[0]);
+        } catch (Exception e) {
+            Log.d("Background Task", e.toString());
+        }
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        ParserTask parserTask = new ParserTask(callerContext);
+
+
+        parserTask.execute(result);
+    }
+
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws Exception {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data; // to be used by onPostExecute()
+    }
+}
