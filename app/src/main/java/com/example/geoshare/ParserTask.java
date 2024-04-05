0ccd49501b348ca4e17502fda7854d0fcef18863
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-04-05 19:40:57 +0700

    refractor: get ParserTask methods from MainActivity

---
+++ app/src/main/java/com/example/geoshare/ParserTask.java
@@ -0,0 +1,79 @@
+package com.example.geoshare;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * A class to parse the Google Places in JSON format
+ */
+public class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+    MainActivity mapContext;
+
+    public ParserTask(Context callerContext){
+        this.mapContext = (MainActivity) callerContext;
+    }
+
+    // Parsing the data in non-ui thread
+    @Override
+    protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+        JSONObject jObject;
+        List<List<HashMap<String, String>>> routes = null;
+
+        try {
+            jObject = new JSONObject(jsonData[0]);
+            DirectionsJSONParser parser = new DirectionsJSONParser();
+
+            routes = parser.parse(jObject);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return routes;
+    }
+
+    @Override
+    protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+        ArrayList points = null;
+        PolylineOptions lineOptions = null;
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        for (int i = 0; i < result.size(); i++) {
+            points = new ArrayList();
+            lineOptions = new PolylineOptions();
+
+            List<HashMap<String, String>> path = result.get(i);
+
+            for (int j = 0; j < path.size(); j++) {
+                HashMap<String, String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                points.add(position);
+            }
+
+            // setting for routes
+            lineOptions.addAll(points);
+            lineOptions.width(12);
+            lineOptions.color(Color.RED);
+            lineOptions.geodesic(true);
+
+        }
+        // Drawing polyline in the Google Map for the i-th route
+        mapContext.getMaps().addPolyline(lineOptions);
+    }
+}
+
