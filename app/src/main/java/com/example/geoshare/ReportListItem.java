bdd1edf6fd0550b0f704b46a5ed29eab6e6953b4
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-04-23 16:57:03 +0700

    add more report attributes and methods

--- app/src/main/java/com/example/geoshare/ReportListItem.java
+++ app/src/main/java/com/example/geoshare/ReportListItem.java
@@ -1,15 +1,21 @@
 package com.example.geoshare;
 
 public class ReportListItem {
-    private String userName;
+    private String receiverName;
+    private String senderId;
+    private String timestamp;
     private int imageId;
 
-    public ReportListItem(String userName, int imageId) {
-        this.userName = userName;
+    public ReportListItem(String receiverName, String senderId, String timestamp, int imageId) {
+        this.receiverName = receiverName;
+        this.senderId = senderId;
+        this.timestamp = timestamp;
         this.imageId = imageId;
     }
 
     // Getter và Setter
-    public String getUserName() { return userName; }
+    public String getReceiverName() { return receiverName; }
+    public String getSenderId(){return senderId;}
+    public String getTimestamp() {return timestamp;}
     public int getImageId() { return imageId; }
 }
