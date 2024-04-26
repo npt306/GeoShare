e4664a13eaa062ad538ab534849d597270cc46ec
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-04-26 22:42:09 +0700

    add image

--- app/src/main/java/com/example/geoshare/AdminReportDetail.java
+++ app/src/main/java/com/example/geoshare/AdminReportDetail.java
@@ -1,22 +1,28 @@
 package com.example.geoshare;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.geoshare.Database.RealtimeDatabase.RealtimeDatabase;
+import com.example.geoshare.Database.Storage.Storage;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
-import java.io.Serializable;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AdminReportDetail extends AppCompatActivity{
     private Report chosenItem;
@@ -74,15 +80,17 @@ public class AdminReportDetail extends AppCompatActivity{
         TextView reportTime = findViewById(R.id.reportTime);
         TextView reportDetail = findViewById(R.id.user_reportDetail);
         TextView reportDescription = findViewById(R.id.reportDescription);
+        CircleImageView userImage = findViewById(R.id.user_Avatar);
 
         DatabaseReference receiverRef = RealtimeDatabase.getInstance()
-                .getUsersReference().child(chosenItem.getReceiverId()).child("username");
+                .getUsersReference().child(chosenItem.getReceiverId());
         receiverRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String str_userName = snapshot.getValue(String.class);
-                userName.setText(str_userName);
+                // get name
+                String str_userName = snapshot.child("username").getValue(String.class);
 
+                // get problems
                 StringBuilder builder = new StringBuilder();
 
                 for (String problem : chosenItem.getReportProblems()) {
@@ -92,6 +100,21 @@ public class AdminReportDetail extends AppCompatActivity{
                     builder.append(problem);
                 }
 
+                // get image
+                String imageURL = snapshot.child("imageURL").getValue(String.class);
+
+                if(imageURL != null && !Objects.equals(imageURL, "default")) {
+                    StorageReference storageRef = Storage.getInstance().getUsersAvatarReference();
+                    storageRef.child(imageURL).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Glide.with(getApplicationContext()).load(uri).into(userImage);
+                        }
+                    });
+                }
+
+                // upload UI elements
+                userName.setText(str_userName);
                 reportTime.setText(chosenItem.getTimestamp());
                 reportDetail.setText(builder.toString());
                 reportDescription.setText(chosenItem.getReportDescription());
